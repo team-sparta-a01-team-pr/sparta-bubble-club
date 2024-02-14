dffc7c2f71664aa0c3b34f5e4a58aa50fb19d422
SEUNGHAN BAEK <skud11311@gmail.com>
2024-02-14 14:39:33 +0900

    project init

---
+++ src/main/kotlin/com/sparta/bubbleclub/SpartaBubbleClubApplication.kt
@@ -0,0 +1,11 @@
+package com.sparta.bubbleclub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpartaBubbleClubApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpartaBubbleClubApplication>(*args)
+}
---
+++ src/test/kotlin/com/sparta/bubbleclub/SpartaBubbleClubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sparta.bubbleclub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpartaBubbleClubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
