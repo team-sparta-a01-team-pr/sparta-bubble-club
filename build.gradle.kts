25d56f7cacbfcee19aa39c6c7c2463c123b4a5e1
rugii913 <rugii913@gmail.com>
2024-02-16 17:07:41 +0900

    Add: spring-boot-starter-validation 종속성 추가
    - spring-boot-starter-validation 종속성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ val queryDslVersion = "5.0.0"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
