5a649732341a54364e53e3ca9934417239d8405b
SEUNGHAN BAEK <skud11311@gmail.com>
2024-02-21 16:34:38 +0900

    refactor: Redis 캐시 적용 리팩토링
    - BubbleService 정책에 맞게 @Cacheable, @CacheEvict 적용
    - BubbleService CacheKey.kt 적용으로 인한 수정
    - cacheManger valueSerializer(String -> Jdk) 변경
    - 메서드 명 변경을 통해 Bean 이름을 설정하여 Bean(name) 설정을 삭제함

--- src/main/kotlin/com/sparta/bubbleclub/domain/keyword/service/KeywordService.kt
+++ src/main/kotlin/com/sparta/bubbleclub/domain/keyword/service/KeywordService.kt
@@ -4,6 +4,7 @@ import com.sparta.bubbleclub.domain.bubble.dto.request.SearchKeywordRequest
 import com.sparta.bubbleclub.domain.keyword.dto.response.KeywordResponse
 import com.sparta.bubbleclub.domain.keyword.entity.KeywordStore
 import com.sparta.bubbleclub.domain.keyword.repository.KeywordRepository
+import com.sparta.bubbleclub.infra.redis.CacheKey.POPULAR_KEYWORDS
 import jakarta.transaction.Transactional
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.scheduling.annotation.Scheduled
@@ -18,38 +19,34 @@ class KeywordService(
 
     private val zSetOperations = redisTemplate.opsForZSet()
 
-    companion object {
-        private const val REDIS_KEY = "keyword"
-    }
-
     @Transactional
     fun increaseKeywordCount(request: SearchKeywordRequest) {
         // redis에 이 keyword 있는지 검사, 없으면 추가
-        zSetOperations.addIfAbsent(REDIS_KEY, request.keyword, 0.0)
+        zSetOperations.addIfAbsent(POPULAR_KEYWORDS, request.keyword, 0.0)
 
         // score ++
         increaseScore(request.keyword)
     }
 
     fun getPopularKeywords(): List<KeywordResponse> {
-        val rank = zSetOperations.reverseRange(REDIS_KEY, 0, 9) as Set<String>?
+        val rank = zSetOperations.reverseRange(POPULAR_KEYWORDS, 0, 9) as Set<String>?
         return rank?.map { KeywordResponse.toResponse(it) } ?: keywordRepository.getPopularKeywords()
     }
 
     @Transactional
     @Scheduled(fixedDelay = 1000 * 60 * 10)
     fun flushAllCache() {
-        zSetOperations.reverseRangeWithScores(REDIS_KEY, 0, 9)
+        zSetOperations.reverseRangeWithScores(POPULAR_KEYWORDS, 0, 9)
             .let {
                 it?.map { keyword -> KeywordStore(keyword.value!!, keyword.score!!.toLong(), ZonedDateTime.now()) }
             }
             ?.also { keywords -> keywordRepository.saveAll(keywords) }
 
-        zSetOperations.removeRange(REDIS_KEY, 0, zSetOperations.size(REDIS_KEY)?.minus(1) ?: 0)
+        zSetOperations.removeRange(POPULAR_KEYWORDS, 0, zSetOperations.size(POPULAR_KEYWORDS)?.minus(1) ?: 0)
     }
 
     private fun increaseScore(keyword: String) {
-        zSetOperations.incrementScore(REDIS_KEY, keyword, 1.0)
+        zSetOperations.incrementScore(POPULAR_KEYWORDS, keyword, 1.0)
     }
 
 }
