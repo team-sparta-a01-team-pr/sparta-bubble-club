102e239c379e3804d462b5fa91a45faf8f0d8e0f
SEUNGHAN BAEK <skud11311@gmail.com>
2024-02-21 20:04:46 +0900

    test: V1 테스트용 브랜치 등록
    - V1 컨트롤러 그대로 사용
    - 인기 검색어 목록 조회 Cache -> RDB 변경 적용

--- src/main/kotlin/com/sparta/bubbleclub/domain/keyword/service/KeywordService.kt
+++ src/main/kotlin/com/sparta/bubbleclub/domain/keyword/service/KeywordService.kt
@@ -2,49 +2,26 @@ package com.sparta.bubbleclub.domain.keyword.service
 
 import com.sparta.bubbleclub.domain.bubble.dto.request.SearchKeywordRequest
 import com.sparta.bubbleclub.domain.keyword.dto.response.KeywordResponse
-import com.sparta.bubbleclub.domain.keyword.entity.KeywordStore
 import com.sparta.bubbleclub.domain.keyword.repository.KeywordRepository
-import com.sparta.bubbleclub.infra.redis.CacheKey.POPULAR_KEYWORDS
 import jakarta.transaction.Transactional
-import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
-import java.time.ZonedDateTime
 
 @Service
 class KeywordService(
     private val keywordRepository: KeywordRepository,
-    private val redisTemplate: RedisTemplate<String, String>,
 ) {
-
-    private val zSetOperations = redisTemplate.opsForZSet()
 
     @Transactional
     fun increaseKeywordCount(request: SearchKeywordRequest) {
-        // redis에 이 keyword 있는지 검사, 없으면 추가
-        zSetOperations.addIfAbsent(POPULAR_KEYWORDS, request.keyword, 0.0)
+        val keyword = request.toEntity()
 
-        // score ++
-        increaseScore(request.keyword)
+        keyword.increaseCount()
+
+        keywordRepository.save(keyword)
     }
 
     fun getPopularKeywords(): List<KeywordResponse> {
-        val rank = zSetOperations.reverseRange(POPULAR_KEYWORDS, 0, 9) as Set<String>?
-        return rank?.map { KeywordResponse.toResponse(it) } ?: keywordRepository.getPopularKeywords()
-    }
-
-    @Transactional
-    @Scheduled(fixedDelay = 1000 * 60 * 10, initialDelay = 1000 * 60 * 10)
-    fun flushAllCache() {
-        zSetOperations.reverseRangeWithScores(POPULAR_KEYWORDS, 0, 9)
-            ?.map { KeywordStore(it.value!!, it.score!!.toLong(), ZonedDateTime.now()) }
-            ?.also { keywords -> keywordRepository.saveAll(keywords) }
-
-        zSetOperations.removeRange(POPULAR_KEYWORDS, 0, zSetOperations.size(POPULAR_KEYWORDS)?.minus(1) ?: 0)
-    }
-
-    private fun increaseScore(keyword: String) {
-        zSetOperations.incrementScore(POPULAR_KEYWORDS, keyword, 1.0)
+        return keywordRepository.getPopularKeywords()
     }
 
 }
