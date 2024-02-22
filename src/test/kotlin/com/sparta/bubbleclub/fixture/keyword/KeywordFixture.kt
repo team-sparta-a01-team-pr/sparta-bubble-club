66770cbedd3716728f55ca2d98e2182c9cada181
umin <adorecamus@gmail.com>
2024-02-22 18:04:20 +0900

    Test: KeywordRepository getPopularKeywords() 테스트
    - 15개의 KeywordStore list fixture 생성

--- src/test/kotlin/com/sparta/bubbleclub/fixture/keyword/KeywordFixture.kt
+++ src/test/kotlin/com/sparta/bubbleclub/fixture/keyword/KeywordFixture.kt
@@ -1,7 +1,19 @@
 package com.sparta.bubbleclub.fixture.keyword
 
 import com.appmattus.kotlinfixture.kotlinFixture
+import com.sparta.bubbleclub.domain.keyword.entity.KeywordStore
+import java.time.ZonedDateTime
 
 object KeywordFixture {
-    val fixture = kotlinFixture()
+
+    val fixture = kotlinFixture {
+        repeatCount { 15 }
+    }
+
+    val keywordList = fixture<List<KeywordStore>> {
+        generate()
+    }
+
+    fun generate(keyword: String = fixture(), count: Long = fixture(), createdAt: ZonedDateTime = fixture()) = KeywordStore(keyword, count, createdAt)
+
 }
